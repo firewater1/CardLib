3ede8eaae8f1414ca78a8a7a37d2c74b0aa41be7
yanlong ning <382145385@qq.com>
2019-05-29 12:02:14 +0800

    添加项目文件。

---
+++ Ch10CardLib/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch10CardLib
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Card GetCard(int cardNum)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
