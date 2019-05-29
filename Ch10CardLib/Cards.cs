33cc4c60ee740de1e4488500f3f44443353270fe
yanlong ning <382145385@qq.com>
2019-05-29 17:34:49 +0800

    添加Cards集合，实现Deck类

---
+++ Ch10CardLib/Cards.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+namespace Ch10CardLib
+{
+    public class Cards : CollectionBase
+    {
+        public void Add(Card card)
+        {
+            List.Add(card);
+        }
+        public void Remove(Card oldCard)
+        {
+            List.Remove(oldCard);
+        }
+        public Card this[int cardIndex]
+        {
+            get { return (Card)List[cardIndex]; }
+            set { List[cardIndex] = value; }
+        }
+        public void CopyTo(Cards targetCards)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                targetCards[index] = this[index];
+            }
+        }
+    }
+}
--- Ch10CardLib/Deck.cs
+++ Ch10CardLib/Deck.cs
@@ -7,14 +7,53 @@ namespace Ch10CardLib
 {
     public class Deck
     {
+        private Card[] cards;
         public Deck()
         {
-            throw new System.NotImplementedException();
+            cards = new Card[52];
+            for (int suitVal = 0; suitVal < 4; suitVal++)
+            {
+                for (int rankVal = 0; rankVal < 14; rankVal++)
+                {
+                    cards[suitVal * 13 + rankVal - 1] = new Card((Suit)suitVal, (Rank)rankVal);
+                }
+            }
         }
 
         public Card GetCard(int cardNum)
         {
-            throw new System.NotImplementedException();
+            if (cardNum >= 0 && cardNum <= 51)
+            {
+                return cards[cardNum];
+            }
+            else
+            {
+                throw (new System.ArgumentOutOfRangeException("carNum", cardNum, "Value must be between 0 and 51."));
+            }
+        }
+
+        public void Shuffle()
+        {
+            Card[] newDeck = new Card[52];
+            bool[] assigned = new bool[52];
+            Random sourceGen = new Random();
+            for (int i = 0; i < 52; i++)
+            {
+                int destCard = 0;
+                bool foundCard = false;
+                while (foundCard == false)
+                {
+                    destCard = sourceGen.Next(52);
+                    if (assigned[destCard] == false)
+                    {
+                        foundCard = true;
+                    }
+                }
+                assigned[destCard] = true;
+                newDeck[destCard] = cards[i];
+            }
+            newDeck.CopyTo(cards, 0);
+        
         }
     }
 }
