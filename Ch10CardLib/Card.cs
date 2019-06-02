f559ac042890933335964be511f26103476f217d
yanlong ning <382145385@qq.com>
2019-06-02 16:28:14 +0800

    给CardLib添加运算符重载完成

--- Ch10CardLib/Card.cs
+++ Ch10CardLib/Card.cs
@@ -44,5 +44,83 @@ namespace Ch10CardLib
             return 13 * (int)suit + (int)rank;
         }*/
         public override int GetHashCode() =>  13 * (int) suit + (int) rank;
+        public static bool operator >(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)
+                {
+                    if (card1.rank == Rank.Ace)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (card2.rank == Rank.Ace)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return (card1.rank > card2.rank);
+                        }
+                    }
+                }
+                else
+                {
+                    return (card1.rank > card2.rank);
+                }
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trumps))
+                {
+                    return false;
+                }
+                else
+                    return true;
+            }
+        }
+        public static bool operator <(Card card1, Card card2) => !(card1 > card2);
+        public static bool operator >=(Card card1, Card card2)
+        {
+            if (card1.suit == card2.suit)
+            {
+                if (isAceHigh)
+                {
+                    if (card1.rank == Rank.Ace)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (card2.rank == Rank.Ace)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return (card1.rank >= card2.rank);
+                        }
+                    }
+                }
+                else
+                {
+                    return (card1.rank >= card2.rank);
+                }
+            }
+            else
+            {
+                if (useTrumps && (card2.suit == Card.trumps))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        public static bool operator <=(Card card1, Card card2) => !(card1 > card2);
     }
 }
